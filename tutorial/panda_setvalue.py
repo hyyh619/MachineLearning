4a2e210d6096955279b40e4e4d415478c1b005bb
Huang Ying <hyyh619@hotmail.com>
2017-08-07 15:31:22 +0800

    Add tutorial of setting value in pandas.

---
+++ tutorial/panda_setvalue.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 15:20:41 2017
+
+@author: yinghuang
+"""
+
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('20160101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=['a', 'b', 'c', 'd'])
+
+print df
+df.iloc[2, 2] = 101
+print df
+df.loc['2016-01-01', 'a'] = 99
+print df
+
+df.a[df.a<0] = -99
+print df
+
+df['f'] = np.nan
+print df
+
+df['e'] = pd.Series([1, 2, 3, 4, 5, 6], index = pd.date_range('20160101', periods=6))
+print df
